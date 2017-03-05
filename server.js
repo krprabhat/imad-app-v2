2968d51e0c344f063d2dd89b99e3639ab317dc27
krprabhat <prabhatsharmakr@gmail.com>
2017-03-05 16:42:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,64 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone = {
+    title: 'Article One',
+    heading :'This is Article One',
+    date:'feb 5',
+    content: `
+            <p>The viewport is the user's visible area of a web page.The viewport varies with the device, and will be smaller on a              mobile phone than on a computer screen.
+                Before tablets and mobile phones, web pages were designed only for computer screens, and it was common for web pages to have a static design and a fixed size.
+
+                Then, when we started surfing the internet using tablets and mobile phones, fixed size web pages were too large to fit the viewport. To fix this, browsers on those devices scaled down the entire web page to fit the screen.
+
+                This was not perfect!! But a quick fix.
+                </p>`
+};
+    
+function createTemp (data) {   
+    
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+var htmlTemp = `
+<html>
+    <head>
+        <title>${title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="/ui/style.css" rel="stylesheet" />   
+    
+    </head>
+    
+    <body>
+      <div class="cont">
+        <div>
+        <a href='/'>Home</a>
+        </div>
+        <hr/>
+        <div>
+            ${date}
+        </div>
+        <div>
+            <h3>
+          ${heading}
+          </h3>
+        </div>
+       <div> 
+            ${content}
+       </div>
+    </div>
+    </body>        
+</html>
+
+`;
+return htmlTemp;
+
+}
+
+
+
 /**
 //var articles = {
   var article1 = {
@@ -84,8 +142,8 @@ app.get('/', function (req, res) {
 
 
 app.get('/article1',function(req, res)
-{   res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
-  // res.send(createTemp(article1));
+{   //res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
+  res.send(createTemp(articleone));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
