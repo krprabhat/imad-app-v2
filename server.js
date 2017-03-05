88313de7d8a675e89d08a15c245f7ad817d41f74
krprabhat <prabhatsharmakr@gmail.com>
2017-03-05 16:53:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleone = {
+var articles ={ 
+
+articleone : {
     title: 'Article One',
     heading :'This is Article One',
     date:'feb 5',
@@ -16,7 +18,20 @@ var articleone = {
                 Then, when we started surfing the internet using tablets and mobile phones, fixed size web pages were too large to fit the viewport. To fix this, browsers on those devices scaled down the entire web page to fit the screen.
 
                 This was not perfect!! But a quick fix.
-                </p>`
+                </p>`}
+articletwo : {
+     title: 'Article Two',
+    heading :'This is Article 2',
+    date:'feb 6',
+    content: `
+            <p>The viewport is the user's visible area of a web page.The viewport varies with the device, and will be smaller on a              mobile phone than on a computer screen.
+                Before tablets and mobile phones, web pages were designed only for computer screens, and it was common for web pages to have a static design and a fixed size.
+
+                Then, when we started surfing the internet using tablets and mobile phones, fixed size web pages were too large to fit the viewport. To fix this, browsers on those devices scaled down the entire web page to fit the screen.
+
+                This was not perfect!! But a quick fix.
+                </p>`}
+                
 };
     
 function createTemp (data) {   
@@ -63,74 +78,6 @@ return htmlTemp;
 
 
 
-/**
-//var articles = {
-  var article1 = {
-              title :'Article one',
-              heading :'article1',
-              date:'feb 7 2017',
-              conte:`<p>Hello, this is article one</p>
-        <p>The viewport is the user's visible area of a web page.The viewport varies with the device, and will be smaller on a mobile phone than on a computer screen.
-            Before tablets and mobile phones, web pages were designed only for computer screens, and it was common for web pages to have a static design and a fixed size.
-
-             Then, when we started surfing the internet using tablets and mobile phones, fixed size web pages were too large to fit the viewport. To fix this, browsers on those devices scaled down the entire web page to fit the screen.
-
-              This was not perfect!! But a quick fix.</p>`
- // }
-  /** 'article2' : {
-              title :'Article Two',
-              heading :'article2',
-              date:'feb 8 2017',
-              conte:`<p>Hello, this is article two</p>
-        <p>The viewport is the user's visible area of a web page.The viewport varies with the device, and will be smaller on a mobile phone than on a computer screen.
-            Before tablets and mobile phones, web pages were designed only for computer screens, and it was common for web pages to have a static design and a fixed size.
-
-             Then, when we started surfing the internet using tablets and mobile phones, fixed size web pages were too large to fit the viewport. To fix this, browsers on those devices scaled down the entire web page to fit the screen.
-
-              This was not perfect!! But a quick fix.</p>`
-                }
-     
-      };
-
-function createTemp(data) {
-    var title =data.title;
-    var date=data.date;
-    var heading=data.heading;
-    
-    var conte=data.conte;
-    
-var temp =
-    `<html>
-        <head>
-            <title>
-             ${title}
-            </title>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link href = '/ui/style.css'>
-         </head>
-    
-        <body>
-            <div class="cont">
-                <div>
-                <a href='/'>Home</a>
-                </div>
-                 <hr/>
-                <h2>
-                ${heading}
-                </h2>
-                <div>
-                ${date}
-                </div>
-                <div>
-                ${content}
-                </div>
-            </div>
-         </body>        
-    </html>`
-;
-return temp;
-
-}**/ 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
