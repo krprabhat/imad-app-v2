211d4f9a7127ef6d97b5f917452d0a9113994123
krprabhat <prabhatsharmakr@gmail.com>
2017-04-08 19:15:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -47,6 +47,9 @@ function move() {
       width++; 
       elem.style.width = width + '%'; 
       elem.innerHTML = width * 1  + '%';
+      
+      
+      
     }
   }
 } 
@@ -108,4 +111,20 @@ function move4() {
       elem.innerHTML = width * 1  + '%';
     }
   }
+}
+
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2500);    
 }
