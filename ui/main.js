cd8fd783ad064a72df160987fb5c8126a64b8619
krprabhat <prabhatsharmakr@gmail.com>
2017-04-08 18:43:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -50,3 +50,18 @@ function move() {
     }
   }
 } 
+
+function move1() {
+  var elem = document.getElementById("myBar");   
+  var width = 20;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 70) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
